389672c8ce94716fab9b2248b19c9d3b9de1f295
unknown <AKulkarni@AKulkarni-Lap7.4YourSoul.net>
2017-05-06 00:05:56 -0500

    Initial commit

---
+++ src/com/uptake/testcases/HomePage.java
@@ -0,0 +1,90 @@
+package com.uptake.testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HomePage {
+	
+	String baseUrl = "https://uptake.com/";
+	
+	public String getTitle(){
+		
+		String actualTitle = driver.getTitle();
+		
+		return actualTitle;
+	}
+	
+	WebDriver driver;
+
+	public HomePage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver=driver;
+	}
+	
+
+/*** Open the parent home page method ***/
+	
+	public void gotoHomePage(){
+		
+		driver.get(baseUrl);
+	}
+	
+	
+/*** Click on Approach tab on the home page ***/
+	
+	public void gotoApproachTab() {
+		
+		driver.findElement(By.linkText("APPROACH")).click();
+	}
+
+
+/*** Scroll down twice to 'Open Positions' section and click on the button ***/
+	
+	public void gotoOpenPositionsDiv() throws InterruptedException{
+		
+		driver.findElement(By.linkText("APPROACH")).click();
+		
+		Thread.sleep(1000);
+		
+		WebElement searchBtn = driver.findElement(By.tagName("button")); 
+		
+		searchBtn.sendKeys(Keys.PAGE_DOWN);
+		
+		Thread.sleep(1000);
+		
+		searchBtn.sendKeys(Keys.PAGE_DOWN);
+		
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//*[@id=\"approach\"]/div[3]/div/div/div/button")).click();
+		
+		Thread.sleep(2000);
+		
+	}
+
+	
+/*** Open Jobs section page and search for QA Engineer position ***/
+	
+	public void clickOpenJobPositions() throws InterruptedException {
+		
+		gotoOpenPositionsDiv();
+		
+		for (String winHandle : driver.getWindowHandles()) 
+		{
+	           driver.switchTo().window(winHandle);
+		}
+		
+		Thread.sleep(2000);
+			
+			boolean exists = driver.findElements( By.partialLinkText("QA")).size() != 0;
+		
+			if (exists) System.out.println("QA position(s) found");	
+			
+			driver.findElement(By.linkText("QA Engineer")).click();
+
+			Thread.sleep(2000);
+		}
+
+}
---
+++ src/com/uptake/testcases/VerifyPages.java
@@ -0,0 +1,117 @@
+package com.uptake.testcases;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.*;
+import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@SuppressWarnings("unused")
+
+public class VerifyPages {
+	
+	public String baseUrl = "https://uptake.com/";
+	
+	WebDriver driver;	
+	
+	private HomePage homepage; 
+
+
+@BeforeTest
+	
+	public void setBaseURL() {
+	
+		System.setProperty("webdriver.chrome.driver", "C:/chromedriver_win32/chromedriver.exe");
+	
+		driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get(baseUrl);
+		
+		homepage = new HomePage(driver);
+	}
+	
+	
+@Test(priority = 0)
+
+	public void verifyHomePageTitle() {		
+		
+		String expectedTitle = "Analytics for the industrial internet";
+		
+		String actualTitle = homepage.getTitle();
+		
+		Assert.assertEquals(expectedTitle,actualTitle);
+		
+		homepage.gotoApproachTab();
+}
+	
+
+@Test(priority = 1)
+
+	public void navigatetoApproachTab() throws InterruptedException{
+
+		homepage.gotoApproachTab();
+		
+		Thread.sleep(1000);
+		
+		String expected = "Approach";
+		
+		String actual = homepage.getTitle();
+		
+		Assert.assertEquals(actual, expected);
+	}
+	
+	
+@Test(priority = 2)
+
+	public void gotoOpenPositionsDiv() throws InterruptedException{
+		
+		String expected = "Approach";
+		
+		String actual = homepage.getTitle();
+		
+		Assert.assertEquals(actual, expected);
+				
+	}
+	
+	
+@Test(priority = 3)
+	
+	public void searchInJobListings() throws InterruptedException{
+			
+		Thread.sleep(1000);
+		
+		homepage.clickOpenJobPositions();
+		
+		//driver.getWindowHandle();
+		
+		String expected = "Job Application for QA Engineer at Uptake";
+		
+		String actual = homepage.getTitle();
+		
+		Assert.assertEquals(actual, expected);
+						
+	}
+	
+	
+@AfterTest
+
+	public void endSession(){
+    
+		driver.quit();
+     }
+	
+	
+}
